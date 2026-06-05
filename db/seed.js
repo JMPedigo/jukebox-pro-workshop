50a19a29c11fbdf14ad27ba6c9bc8d1f1fbb4fd9
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-05 11:13:19 -0500

    changes seed file to include users...had to restructure since playlist requires a user now

--- db/seed.js
+++ db/seed.js
@@ -3,19 +3,27 @@ import db from "#db/client";
 import { createPlaylist } from "#db/queries/playlists";
 import { createPlaylistTrack } from "#db/queries/playlists_tracks";
 import { createTrack } from "#db/queries/tracks";
+import { createUser } from "#db/queries/users";
 
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
+/** Update the seed file to additionally seed at least 2 users.
+ *  Each user should have a playlist containing at least 5 tracks.
+ *  You will have to edit how playlists are seeded as well, since they now are required to belong to a user!
+ */
 async function seed() {
   for (let i = 1; i <= 20; i++) {
-    await createPlaylist("Playlist " + i, "lorem ipsum playlist description");
     await createTrack("Track " + i, i * 50000);
   }
-  for (let i = 1; i <= 15; i++) {
-    const playlistId = 1 + Math.floor(i / 2);
-    await createPlaylistTrack(playlistId, i);
+
+  for (let i = 1; i <= 5; i++) {
+    const user = await createUser("user" + i, "password");
+    await createPlaylist("playlist" + i, "description" + i, user.id);
+    for (let j = 0; j < 5; j++) {
+      await createPlaylistTrack(user.id, (i - 1) * 5 + j + 1);
+    }
   }
 }
