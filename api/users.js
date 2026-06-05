be49489bcbfe2880b089cc2f0cf2108c593cb617
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-05 12:49:13 -0500

    Adds POST /users/register to create a user

--- api/users.js
+++ api/users.js
@@ -6,13 +6,18 @@ import { createUser } from "#db/queries/users";
 import requireBody from "#middleware/requireBody";
 import { createToken } from "#utils/jwt";
 
+/**POST /users/register creates a new user with the provided credentials
+ * sends 400 if request body is missing username or password
+ * make sure passwords are hashed!
+ * creates and sends a token with the ID of the created user
+ */
 router.post(
   "/register",
   requireBody(["username", "password"]),
   async (req, res) => {
     const { username, password } = req.body;
     const user = await createUser(username, password);
-    const token = createToken({ id: user.id });
+    const token = await createToken({ id: user.id });
     res.status(201).send(token);
   },
 );
