fa8480a5233738dc11a0c6f3c6fdba41484ebeb9
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-05 11:51:20 -0500

    Imports createUser, and createToken

--- api/users.js
+++ api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 export default router;
 
 import { createUser } from "#db/queries/users";
+import requireBody from "#middleware/requireBody";
+import { createToken } from "#utils/jwt";
 
 router.post(
   "/register",
