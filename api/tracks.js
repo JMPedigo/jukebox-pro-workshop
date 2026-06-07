481be3c97a5a4794d9d6d8141032b80d15ad6c64
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-06 19:52:41 -0500

    Adds router.params and reformats functions to require userid

--- api/tracks.js
+++ api/tracks.js
@@ -3,14 +3,27 @@ const router = express.Router();
 export default router;
 
 import { getTracks, getTrackById } from "#db/queries/tracks";
+import requireUser from "#middleware/requireUser";
+import { getPlaylistByTrackId } from "#db/queries/playlists";
 
 router.get("/", async (req, res) => {
   const tracks = await getTracks();
   res.send(tracks);
 });
 
-router.get("/:id", async (req, res) => {
-  const track = await getTrackById(req.params.id);
+/** I need to add router.param now, since I am reusing the logic for id */
+router.param("id", async (req, res, next, id) => {
+  const track = await getTrackById(id);
   if (!track) return res.status(404).send("Track not found.");
-  res.send(track);
+  req.track = track;
+  next();
+});
+
+router.get("/:id", (req, res) => {
+  res.send(req.track);
+});
+
+router.get("/:id/playlists", requireUser, async (req, res) => {
+  const playlists = await getPlaylistByTrackId(req.track.id);
+  res.send(playlists);
 });
