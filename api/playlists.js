0e667be8e1b84bab73eb5407066a3cf03b6e3ce8
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-07 11:29:15 -0500

    Reformats GET playlists/:id/tracks to requireBody

--- api/playlists.js
+++ api/playlists.js
@@ -27,10 +27,15 @@ router.post("/", requireBody(["name", "description"]), async (req, res) => {
   const playlist = await createPlaylist(name, description, req.user.id);
   res.status(201).send(playlist);
 });
-
+/** GET /playlists/:id sends 403 error if the user does not own the playlist. */
 router.param("id", async (req, res, next, id) => {
   const playlist = await getPlaylistById(id);
   if (!playlist) return res.status(404).send("Playlist not found.");
+
+  if (playlist.user_id !== req.user.id)
+    return res
+      .status(403)
+      .send("You cannot access playlists owned by another user.");
 
   req.playlist = playlist;
   next();
@@ -40,17 +45,14 @@ router.get("/:id", (req, res) => {
   res.send(req.playlist);
 });
 
+/**  */
 router.get("/:id/tracks", async (req, res) => {
   const tracks = await getTracksByPlaylistId(req.playlist.id);
   res.send(tracks);
 });
 
-router.post("/:id/tracks", async (req, res) => {
-  if (!req.body) return res.status(400).send("Request body is required.");
-
+router.post("/:id/tracks", requireBody(["trackId"]), async (req, res) => {
   const { trackId } = req.body;
-  if (!trackId) return res.status(400).send("Request body requires: trackId");
-
   const playlistTrack = await createPlaylistTrack(req.playlist.id, trackId);
   res.status(201).send(playlistTrack);
 });
